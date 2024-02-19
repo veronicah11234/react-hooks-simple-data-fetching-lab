ef313df035b9eb5b19e57aadb91a19ee59b4dcb9
miriam <miriamwangui525@gmail.com>
2024-02-19 16:01:37 +0300

    displaying dogs randomly using usestate

---
+++ src/App.js
@@ -0,0 +1,36 @@
+// import React, { useState, useEffect } from 'react';
+
+// const App = () => {
+//   const [dogImage, setDogImage] = useState(null);
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await fetch('https://dog.ceo/api/breeds/image/random');
+//         const data = await response.json();
+//         setDogImage(data.message);
+//         setLoading(false);
+//       } catch (error) {
+//         console.error('Error fetching data:', error);
+//         setLoading(false);
+//       }
+//     };
+
+//     fetchData();
+//   }, []); // The empty dependency array ensures that useEffect runs only once, similar to componentDidMount
+
+//   return (
+//     <div>
+//       {loading ? (
+//         <p>Loading...</p>
+//       ) : (
+//         <div>
+//           <img src={dogImage} alt="A Random Dog" />
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default App;
